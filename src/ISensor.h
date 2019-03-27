2737c7dda597ee16459de6cf12061cd8e5655b0a
Nikfinn99 <43323055+Nikfinn99@users.noreply.github.com>
2019-03-27 22:55:23 +0100

    fix indent

--- src/ISensor.h
+++ src/ISensor.h
@@ -5,68 +5,68 @@
 
 class ISensor
 {
-  private:
-    unsigned long m_meas_interval, m_last_measurement;
-    MQTT_Client *m_mqtt_client;
+private:
+  unsigned long m_meas_interval, m_last_measurement;
+  MQTT_Client *m_mqtt_client;
 
-  protected:
-    /**
+protected:
+  /**
      * publish method for sensor measurements
      * ensures that client has been initialized
      * 
      * @param topic sub_topic for mqtt message
      * @param message sensor measurement to publish
     */
-    ISensor &publish(const String &p_topic, const String &p_message)
+  ISensor &publish(const String &p_topic, const String &p_message)
+  {
+    //check if mqtt_client has been initialized
+    if (m_mqtt_client)
     {
-        //check if mqtt_client has been initialized
-        if (m_mqtt_client)
-        {
-            // publish message to topic
-            m_mqtt_client->publish(p_topic, p_message);
-        }
-        return *this;
+      // publish message to topic
+      m_mqtt_client->publish(p_topic, p_message);
     }
+    return *this;
+  }
 
-    /**
+  /**
      * interface method for sensor measurement
      * only for use inside class
     */
-    virtual void measure() = 0;
+  virtual void measure() = 0;
 
-  public:
-    ISensor(unsigned long p_interval) : m_meas_interval(p_interval), m_last_measurement(0), m_mqtt_client(nullptr) {}
+public:
+  ISensor(unsigned long p_interval) : m_meas_interval(p_interval), m_last_measurement(0), m_mqtt_client(nullptr) {}
 
-    ~ISensor() {}
+  ~ISensor() {}
 
-    /**
+  /**
      * attach existing MQTT_Client to sensor
      * enables publishing of messages
      * 
      * @param client mqtt_client to attach to sensor
     */
-    ISensor &setClient(MQTT_Client *p_client)
-    {
-        m_mqtt_client = p_client;
-        return *this;
-    }
+  ISensor &setClient(MQTT_Client *p_client)
+  {
+    m_mqtt_client = p_client;
+    return *this;
+  }
 
-    /**
+  /**
      * update method for sensor
      * this will call the overridden measurement function every interval
      * call this method regulary to ensure correct functionality
     */
-    ISensor &update()
+  ISensor &update()
+  {
+    // check if interval has expired
+    if (millis() > m_last_measurement + m_meas_interval)
     {
-        // check if interval has expired
-        if (millis() > m_last_measurement + m_meas_interval)
-        {
-            // update last measurment time
-            m_last_measurement = millis();
+      // update last measurment time
+      m_last_measurement = millis();
 
-            // call overridden method
-            measure();
-        }
-        return *this;
+      // call overridden method
+      measure();
     }
+    return *this;
+  }
 };
