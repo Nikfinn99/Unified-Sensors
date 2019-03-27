4ba0a4516eee1d9ee6a4e045ac7b2e326b426fea
Nikfinn99 <43323055+Nikfinn99@users.noreply.github.com>
2019-03-27 09:38:49 +0100

    create sensor Interface and DHT sensor

---
+++ src/DHT_Sensor.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "ISensor.h"
+#include <DHT.h>
+
+#ifndef DHT_MEAS_INTERVAL
+#define DHT_MEAS_INTERVAL 10 * 60 * 1000
+#endif
+
+class DHT_Sensor : public ISensor
+{
+  private:
+    DHT *m_sensor;
+
+  public:
+    DHT_Sensor(int p_pin, int p_type, unsigned long p_interval = DHT_MEAS_INTERVAL) : ISensor(p_interval)
+    {
+        // create new dht object attached to corresponding pins
+        m_sensor = new DHT(p_pin, p_type);
+        m_sensor->begin();
+    }
+    ~DHT_Sensor()
+    {
+        // cleanup
+        delete m_sensor;
+    }
+
+    /**
+     * implement measurement function for dht sensor
+    */
+    virtual void measure() override
+    {
+        // measure sensor readings
+        float temp = m_sensor->readTemperature();
+        float hum = m_sensor->readHumidity();
+
+        // set to -1 if sensor measurement was unsucessfull
+        if (isnan(temp))
+            temp = -1;
+        if (isnan(hum))
+            hum = -1;
+
+        // publish sensor readings to mqtt
+        ISensor::publish("temperature", String(temp));
+        ISensor::publish("humidity", String(hum));
+    }
+};
---
+++ src/ISensor.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <Arduino.h>
+#include <MQTT_Client.h>
+
+class ISensor
+{
+  private:
+    unsigned long m_meas_interval, m_last_measurement;
+    MQTT_Client *m_mqtt_client;
+
+  protected:
+    /**
+     * publish method for sensor measurements
+     * ensures that client has been initialized
+     * 
+     * @param topic sub_topic for mqtt message
+     * @param message sensor measurement to publish
+    */
+    ISensor &publish(const String &p_topic, const String &p_message)
+    {
+        //check if mqtt_client has been initialized
+        if (m_mqtt_client)
+        {
+            // publish message to topic
+            m_mqtt_client->publish(p_topic, p_message);
+        }
+        return *this;
+    }
+
+    /**
+     * interface method for sensor measurement
+     * only for use inside class
+    */
+    virtual void measure() = 0;
+
+  public:
+    ISensor(unsigned long p_interval) : m_meas_interval(p_interval), m_last_measurement(0), m_mqtt_client(nullptr) {}
+
+    ~ISensor() {}
+
+    /**
+     * attach existing MQTT_Client to sensor
+     * enables publishing of messages
+     * 
+     * @param client mqtt_client to attach to sensor
+    */
+    ISensor &setClient(MQTT_Client *p_client)
+    {
+        m_mqtt_client = p_client;
+        return *this;
+    }
+
+    /**
+     * update method for sensor
+     * this will call the overridden measurement function every interval
+     * call this method regulary to ensure correct functionality
+    */
+    ISensor &update()
+    {
+        // check if interval has expired
+        if (millis() > m_last_measurement + m_meas_interval)
+        {
+            // update last measurment time
+            m_last_measurement = millis();
+
+            // call overridden method
+            measure();
+        }
+        return *this;
+    }
+};
